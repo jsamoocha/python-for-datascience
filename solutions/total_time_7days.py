e18707082ed8a7fb2a7e6ecbdcfcefa367939b42
Jonatan Samoocha <jonatan@fithaxx.com>
2020-10-07 16:30:06 +0200

    Corrects column name

--- solutions/total_time_7days.py
+++ solutions/total_time_7days.py
@@ -2,7 +2,7 @@
     activity_df
     .set_index('start_date_local')
     .sort_index()
-    .assign(total_time_7d=lambda df: df.rolling('7d')['elapsed_time'].sum() / 3600)
+    .assign(total_time_7days=lambda df: df.rolling('7d')['elapsed_time'].sum() / 3600)
     .sort_values('total_time_7days', ascending=False)
     .head(10)
 )
